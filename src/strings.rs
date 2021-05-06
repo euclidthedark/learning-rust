f9d8813dcaefb45e1093e9e69acf1b0c0830f005
euclidthedark <euclidthedark@gmail.com>
2021-05-06 19:13:36 -0400

    remove EOF space

--- src/strings.rs
+++ src/strings.rs
@@ -15,22 +15,4 @@ fn main () {
     let first_name = &full_name[0..6];
 
     println!("Yes, {} is my first name.", first_name);
-
-    /*
-     * There are 4 scaler types:
-     * 1) Bool
-     * 2) Ints - Can be signed or unsigned
-     * 3) Chars
-     * 4) And double floats
-     * */
-
-    let is_a_bool = true;
-
-    let an_integer: i8 = 10;
-
-    let a_char = 'a';
-
-    let a_float: f32 = 4.5;
-
-    format!("{}, {}, {}, {}", is_a_bool, an_integer, a_char, a_float);
 }
