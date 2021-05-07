1fb36522a4fd8cb790a75ae5675f9bf2eb6ec160
euclidthedark <euclidthedark@gmail.com>
2021-05-06 20:29:55 -0400

    add if else

---
+++ src/flow_control/if_else.rs
@@ -0,0 +1,11 @@
+fn main () {
+    let a:u8 = 5;
+
+    if a == 5 {
+        println!("It's five.");
+    } else if a==1 {
+        println!("It's 1 too.");
+    } else {
+        println!("This will not print.");
+    }
+}
